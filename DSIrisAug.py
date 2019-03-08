3a995ab0cec136862130b06d7b22323c47581daa
vcvycy <352871242@qq.com>
2019-03-08 12:15:11 +0800

    ImageAugmentation

--- DSIrisAug.py
+++ DSIrisAug.py
@@ -28,7 +28,7 @@ import cv2
 import numpy as np
 import Utils
 import matplotlib.pyplot as plt
-
+import random
 
 # 画点
 def drawPoints(img,points):
@@ -54,12 +54,110 @@ def v4aug(img,points=None):
     img_aug = seq.augment_image(img)
     return img_aug
 
+# 画实心圆
+def drawRandomCircle(img,radius_range,num_range,color_range):
+    num = random.randint(num_range[0],num_range[1])
+    for _ in range(num):
+        h = random.randint(0,img.shape[0]-1)
+        w = random.randint(0,img.shape[1]-1)
+        r = random.randint(radius_range[0],radius_range[1])
+        c = random.randint(color_range[0],color_range[1])
+        cv2.circle(img, (w, h), r, c, thickness=-1)
+    return
+
+def drawRandomLine(img,thick_range,num_range, color_range):
+    num = random.randint(num_range[0],num_range[1])
+    for _ in range(num):
+        h = random.randint(0,img.shape[0]-1)
+        w = random.randint(0,img.shape[1]-1)
+        h2 = random.randint(0,img.shape[0]-1)
+        w2 = random.randint(0,img.shape[1]-1)
+        thick = random.randint(thick_range[0], thick_range[1])
+
+        color = random.randint(color_range[0],color_range[1])
+        cv2.line(img,(w,h),(w2, h2),color,thickness=thick)
+    return
+
+# 虹膜定位图片增强
+# (1) crop (2) 水平翻转 (3) 白块 (4) 黑色线条 (5) 高斯模糊
+def v4locationAug(img , loc, crop_size = (24, 64), scale=2):
+    # 虹膜/瞳孔位置
+    iris = loc["iris"]
+    pupil = loc["pupil"]
+    # 白块(模拟反光)
+    drawRandomCircle(img,(5,8),(0,10),color_range=(180,255))
+    drawRandomCircle(img,(20,50),(0,2),color_range=(180,255))
+
+    # 黑色线条 (模拟眼镜)
+    drawRandomLine(img,(5,10), (0,3), color_range=(0,50))
+
+    # 翻转+模糊+crop
+    seq = iaa.Sequential([
+        # iaa.SomeOf((0,1),   [iaa.Fliplr(0.5)]),        # 一半的概率左右翻转
+        iaa.OneOf([
+            iaa.GaussianBlur((0, 3.0)),
+            iaa.AverageBlur(k=(2, 7)),
+            iaa.MedianBlur(k=(3, 11)),
+        ])
+     ])
+    img = seq.augment_image(img)
+
+    # 左右翻转
+    h, w =img.shape[0],img.shape[1]
+    if  random.randint(0,1) == 0:
+        img = cv2.flip(img,1)
+        pupil["c"][0] = w - 1 - pupil["c"][0]
+        iris["c"][0]  = w - 1 - iris["c"][0]
+
+    # crop
+    nh, nw = (h - crop_size[0], w-crop_size[1])
+    h1 = iris["c"][1] - iris["r"]
+    h2 = iris["c"][1] + iris["r"]
+    w1 = iris["c"][0] - iris["r"]
+    w2 = iris["c"][0] + iris["r"]
+    list_h =[0]
+    for i in range(0,h1):
+        if i + nh -1 >= h2 and i + nh - 1 <= h-1:
+            list_h.append(i)
+    list_w = [0]
+    for i in range(0,w1):
+        if i + nw -1 >=w2 and i + nw -1 <= w-1:
+            list_w.append(i)
+    start_h = list_h[random.randint(0,len(list_h)-1)]
+    start_w = list_w[random.randint(0,len(list_w)-1)]
+    img = img[start_h : start_h + nh, start_w:start_w + nw ]
+    ##
+    pupil["c"][0] -= start_w
+    pupil["c"][1] -= start_h
+    iris["c"][0] -= start_w
+    iris["c"][1] -= start_h
+
+    # reshape
+    h,w = img.shape[0],img.shape[1]
+    img = cv2.resize(img, (w // scale, h // scale), interpolation=cv2.INTER_CUBIC)
+    iris["c"][0] //= scale
+    iris["c"][1] //= scale
+    iris["r"] //= scale
+    pupil["c"][0] //= scale
+    pupil["c"][1] //= scale
+    pupil["r"] //= scale
+    # 显示图片
+    # ##
+    loc = {"iris": iris, "pupil": pupil}
+    # Utils.drawIrisAndShow(img, loc)
+    return img,loc
+
 ###### TEST #########
 if __name__ == "__main__":
-    file = r"E:\虹膜数据集\S5004R04.jpg"
-    img = plt.imread(file)
-    img = v4aug(img)
+    while True:
+        file = r"E:\虹膜数据集\S5004R04.jpg"
+        loc = {"iris": {"c": [319, 216], "r": 99}, "pupil": {"c": [317, 218], "r": 54}}
+        img = plt.imread(file)
+        print(type(img))
+        img = v4locationAug(img,loc)
+        #Utils.showImage(img)
+    #img = v4aug(img)
     #
-    points = [(150,150),(200,150),(200,200),(150,200)]
-    img = drawPoints(img,points)
-    Utils.showImage(img)
+    # points = [(150,150),(200,150),(200,200),(150,200)]
+    # img = drawPoints(img,points)
+    # Utils.showImage(img)
--- Utils.py
+++ Utils.py
@@ -14,8 +14,31 @@ def getFile2Path(root,suffix="jpg"):
     return filename2path
 
 def showImage(mat):
-    cv2.namedWindow("show")
-    cv2.imshow("show",mat)
+    name = "%d x %d" %(mat.shape[0],mat.shape[1])
+    cv2.namedWindow(name)
+    cv2.imshow(name,mat)
     cv2.waitKey(0)
     #ia.imshow(mat)
+    return
+
+def drawAndShowBaseOnCNNOutput(img, o):
+    print(o)
+    h,w = img.shape[0] , img.shape[1]
+    iris_c = int((o[1]+o[3])/2*w),int((o[0]+o[2])/2*h)
+    iris_r = max( int((o[3]-o[1])/2*w) , int((o[2]-o[0])/2*h),0)
+
+    pupil_c = int((o[5]+o[7])/2*w),int((o[4]+o[6])/2*h)
+    pupil_r = max( int((o[7]-o[5])/2*w) , int((o[6]-o[4])/2*h),0)
+    cv2.circle(img, iris_c, iris_r, (255, 0, 255), thickness=3)
+    cv2.circle(img, pupil_c, pupil_r, (255, 0, 255), thickness=3)
+    showImage(img)
+    return
+
+# 在图中画虹膜、瞳孔位置，然后显示出来
+def drawIrisAndShow(img, postion):
+    iris = postion["iris"]
+    pupil = postion["pupil"]
+    cv2.circle(img, tuple(iris["c"]), iris["r"], (255, 0, 255),thickness=3)
+    cv2.circle(img, tuple(pupil["c"]),pupil["r"], (255, 0, 255),thickness=3)
+    showImage(img)
     return
