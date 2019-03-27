bug fix(softmax reduce_mean)

Recognition/ResNet.py
@@ -261,12 +261,12 @@ class ResNet:
             self.one_hot_output = tf.nn.softmax(self.fc(self.embed, config.training_classes, "one_hot_output"))
             self.one_hot_label = tf.placeholder(tf.float32, [None, config.training_classes], name ="one_hot_label")
             self.classfication_loss_weight = tf.placeholder(tf.float32,name =  "classfication_loss_weight")
-            self.classfication_loss = self.classfication_loss_weight * (-tf.reduce_mean(self.one_hot_label * tf.log(tf.clip_by_value(self.one_hot_output,1e-10,1.0))))
+            # self.classfication_loss = self.classfication_loss_weight * (-tf.reduce_mean(self.one_hot_label * tf.log(tf.clip_by_value(self.one_hot_output,1e-10,1.0))))
             # 准确率
 
             self.iscorrect = tf.equal(tf.argmax(self.one_hot_label, 1), tf.argmax(self.one_hot_output, 1), name="iscorrect")
             self.accuracy = tf.reduce_mean(tf.cast(self.iscorrect, dtype=tf.float32), name="accuracy")
-            # self.classfication_loss = self.classfication_loss_weight * tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits= self.one_hot_output, labels=self.one_hot_label))
+            seslf.classfication_loss = self.classfication_loss_weight * tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits= self.one_hot_output, labels=self.one_hot_label))
 
         self.loss = tf.add_n([self.l2_loss, self.trilet_loss,self.classfication_loss], name = "WeightedLoss")
 